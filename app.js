ee46dafd437ff062d35acc5e5c22d806fea2e325
andyxforce707 <andyoshida707@gmail.com>
2025-06-18 13:50:23 +0700

    Initial upload HTML app

---
+++ app.js
@@ -0,0 +1,21 @@
+let link = 'https://jsonplaceholder.typicode.com/posts';
+
+fetch(link).then((res)=>res.json()).then((data)=>{
+    let html = document.querySelector('.data');
+
+    let fragment = '';
+    let style = ''
+    for(let i = 0; i< data.length;i++) {
+        if(i%2 == 0) {
+             style = 'even';
+            fragment += '<span class='+style+'>'+data[i].title.toUpperCase()+'</span><hr>';
+
+        } else {
+            style = 'odd';
+            fragment += '<span class='+style+'>'+data[i].title.toUpperCase()+'</span><hr>';
+        }
+
+    }
+    console.log(fragment);
+    html.innerHTML = fragment;
+});
